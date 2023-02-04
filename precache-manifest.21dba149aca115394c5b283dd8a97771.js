e9dc2b75c34baf2374605362f58fe671beb0d4c8
e0202709 <e0202709@u.nus.edu>
2023-02-04 14:29:44 +0000

    Updates

---
+++ precache-manifest.21dba149aca115394c5b283dd8a97771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4898c4d43f41a679529bbc18b02a98",
+    "url": "/imageGallery/index.html"
+  },
+  {
+    "revision": "791da2bd401d0716c8a9",
+    "url": "/imageGallery/static/css/main.8d8562be.chunk.css"
+  },
+  {
+    "revision": "a7e55521fa03bbc47b7f",
+    "url": "/imageGallery/static/js/2.18fd0567.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/imageGallery/static/js/2.18fd0567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791da2bd401d0716c8a9",
+    "url": "/imageGallery/static/js/main.7223b323.chunk.js"
+  },
+  {
+    "revision": "fd86f2387a3648c0d2d9",
+    "url": "/imageGallery/static/js/runtime-main.ba0ebee1.js"
+  }
+]);
--- src/components/Config.jsx
+++
@@ -1,3 +0,0 @@
-const apiKey = 'tKyRYCR6YbSkvadfIUCe0sa4MEPEtxtX-XTQbuNuVOQ'
-
-export default apiKey
